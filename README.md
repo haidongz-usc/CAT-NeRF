738b28bd2b9e16f3976ac0ea2b2c4cc5d5cbe77c
Haidong <haidongz@usc.edu>
2023-04-03 00:05:03 -0700

    h36m update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CAT-NeRF
-Implementation for CAT-NeRF [CVPRW 2023]
+This repo includes the implementation for CAT-NeRF [CVPRW 2023]. We include our code for H36m dataset.
+
+We follow the implementation of [Animatable NeRF](https://github.com/zju3dv/animatable_nerf). For data preparation and installation, please follow Animatable NeRF for data preparation and evironment setup. We also provide one of our pretrained model for `s11p` for validation of the installation. You can find the corresponding scripts in `train.sh` and `test.sh` as Animatable NeRF. There might be some small differences compared with our reported numbers in the paper due to the reorg of the project.
+
--- train.sh
+++ train.sh
@@ -20,27 +20,3 @@ python train_net.py --cfg_file configs/aninerf_s9p.yaml exp_name aninerf_s9p_ful
 python train_net.py --cfg_file configs/aninerf_s11p.yaml exp_name aninerf_s11p resume False
 python train_net.py --cfg_file configs/aninerf_s11p.yaml exp_name aninerf_s11p_full resume False aninerf_animation True init_aninerf aninerf_s11p
 
-# ZJU-MoCap
-python train_net.py --cfg_file configs/aninerf_313.yaml exp_name aninerf_313 resume False
-python train_net.py --cfg_file configs/aninerf_313.yaml exp_name aninerf_313_full resume False aninerf_animation True init_aninerf aninerf_313
-
-python train_net.py --cfg_file configs/aninerf_315.yaml exp_name aninerf_315 resume False
-python train_net.py --cfg_file configs/aninerf_315.yaml exp_name aninerf_315_full resume False aninerf_animation True init_aninerf aninerf_315
-
-python train_net.py --cfg_file configs/aninerf_377.yaml exp_name aninerf_377 resume False
-python train_net.py --cfg_file configs/aninerf_377.yaml exp_name aninerf_377_full resume False aninerf_animation True init_aninerf aninerf_377
-
-python train_net.py --cfg_file configs/aninerf_386.yaml exp_name aninerf_386 resume False
-python train_net.py --cfg_file configs/aninerf_386.yaml exp_name aninerf_386_full resume False aninerf_animation True init_aninerf aninerf_386
-
-# Extended version: training with S9 of Human3.6M as an example
-
-# Vanilla Animatable NeRF
-python train_net.py --cfg_file configs/aligned_nerf_lbw/aligned_aninerf_lbw_s9p.yaml exp_name aligned_aninerf_lbw_s9p resume False
-python train_net.py --cfg_file configs/aligned_nerf_lbw/aligned_aninerf_lbw_s9p.yaml exp_name aligned_aninerf_lbw_s9p_full resume False aninerf_animation True init_aninerf aligned_aninerf_lbw_s9p
-
-# Pose dependent displacement field + Animatable NeRF
-python train_net.py --cfg_file configs/aligned_nerf_pdf/aligned_aninerf_pdf_s9p.yaml exp_name aligned_aninerf_pdf_s9p resume False
-
-# Pose dependent displacement field + SDF field (full Animatable Neural Fields)
-python train_net.py --cfg_file configs/sdf_pdf/anisdf_pdf_s9p.yaml exp_name anisdf_pdf_s9p resume False
